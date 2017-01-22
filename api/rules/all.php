8452ef5fe2eb6e738d916d65316f096e9a1a8ccb
Joe Jack <thimis88@gmail.com>
2017-01-22 00:38:48 +0000

    trying different headers

--- api/rules/all.php
+++ api/rules/all.php
@@ -1,6 +1,7 @@
 <?PHP
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
 include_once(getcwd() . '/rules.php');
 header('Content-Type: application/json');
 echo json_encode($rules);
