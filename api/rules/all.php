525fdde58d4e229703228b699bb266241869ec1a
Joe Jack <thimis88@gmail.com>
2017-01-22 00:35:56 +0000

    Added backgroud color and fix#2 for front end

--- api/rules/all.php
+++ api/rules/all.php
@@ -1,5 +1,6 @@
 <?PHP
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 include_once(getcwd() . '/rules.php');
 header('Content-Type: application/json');
 echo json_encode($rules);
