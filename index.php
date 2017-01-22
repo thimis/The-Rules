c0d50e8aef991b25aba5afe3547b4038664d4dee
Joe Jack <thimis88@gmail.com>
2017-01-22 00:13:21 +0000

    Added buttons to index

--- index.php
+++ index.php
@@ -9,12 +9,16 @@
             <h3>End Points:</h3>
             <h3>https://rules-thimin.c9users.io/rules/</h3>
             <p1>Returns all of the rules</p1>
+            <a class="button" href="/rules">all rules</a>
             <h3>https://rules-thimin.c9users.io/rules/#</h3>
             <p1>Returns rule number from the passed number</p1>
+            <a class="button" href="/rules/0/">Rule 0</a>
             <h3>https://rules-thimin.c9users.io/rules/random</h3>
             <p1>Returns a random rule.</p1>
+            <a class="button" href="/rules/random">Random Rule</a>
             <h3>https://rules-thimin.c9users.io/rules/range?start=0&end=286</h3>
             <p1>Returns a range of rules set start= and end=  .</p1>
+            <a class="button" href="/rules/range?start=0&end=286">Random Rule</a>
         </body>
     </head>
 </html>
